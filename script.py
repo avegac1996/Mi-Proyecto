cba9c0492fd942fd5b5e64ff96afaf738c1c83d7
Jhossua Vega <anthony_vega95@hotmail.com>
2021-01-04 10:09:45 -0500

    Commit inicial

---
+++ script.py
@@ -0,0 +1,9 @@
+# Script de prueba para Git
+# Autor: Anthony Vega <avegac@est.ups.edu.ec>
+# Fecha: 04-01-2020
+# Versión: 1.0.0
+
+def main():
+	print("Hola Mundo!")
+if __name__ == "__main__":
+	main()
